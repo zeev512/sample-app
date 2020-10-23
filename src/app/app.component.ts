82cf6653997ca624f5600878c61cecbf92f164e7
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 17:03:18 +0300

    Passing template by reference from ng-template directive to ng-container through ngTemplateOutlet usage in latter. Note the use of context to pass in the variables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,32 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 
 /*
-Possible solution would be to replace the outer div element
-with the ng-container element directive.
+Here is the breakdown of this example:
+1. This template, unlike the previous templates also has one input variable (it could also have several)
+2. The input variable is called lessonsCounter , and it's defined via a ng-template property using the pre x let-
+3. The variable lessonsCounter is visible inside the ng-template body, but not outside
+4. The content of this variable is determined by the expression that its assigned to the property let-lessonsCounter
+5. That expression is evaluated against a context object, passed to ngTemplateOutlet together with the template to instantiate
+6. This context object must then have a property named estimate , for any value to be displayed inside the template
+7. The context object is passed to ngTemplateOutlet via the context property, that can receive any expression that evaluates to an object
 
-As we can see, the ng-container directive provides us with an element
-that we can attach a structural directive to.
-This way we don't have to create an extra element just for that.
-But the major use case for the ng-container directive is that it can
-also provide a placeholder for injecting a template dynamically into the
-page.
+This is what would get rendered to the screen:
+Approximately 10 lessons ...
  */
 @Component({
 	selector: 'app-root',
 	template: `
-		<ng-container *ngIf="lessons">
-			<div class="lesson" *ngFor="let lesson of lessons">
-				<div class="lesson-detail">
-					{{lesson | json}}
-				</div>
+		<ng-template #estimateTemplate let-lessonsCounter="estimate">
+			<div> Approximately {{lessonsCounter}} lessons ...
 			</div>
-		</ng-container>	`})
+		</ng-template>
+		<ng-container *ngTemplateOutlet="estimateTemplate;context:ctx">
+		</ng-container>
+	`
+})
 export class AppComponent {
 
-	loginText = 'Login';
+	/*loginText = 'Login';
 	signUpText = 'Sign Up';
 	lessons = ['Lesson 1', 'Lessons 2'];
 
@@ -33,6 +36,8 @@ export class AppComponent {
 
 	signUp() {
 		console.log('Sign Up');
-	}
+	}*/
+	totalEstimate = 10;
+	ctx = {estimate: this.totalEstimate};
 
 }
