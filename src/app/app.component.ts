d9b6404d5fa319d217dd650c8caf8c4c74794bba
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 16:53:12 +0300

    Adding extra div element to solve the issue of applying 2 structural directive on the same element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,19 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-
 /*
-When we use *ngIf structural directive the Angular internally desugars the more concise
-*ngIf structural directive syntax. Let's break down what happened during the desugaring:
-- The element onto which the structural directive ngIf was applied has been moved into an ng-template
-- The expression of *ngIf has been split up and applied to two separate directives, using the [ngIf] and [ngIfElse]
-  template input variable syntax
-Exactly the same process occurs with with *ngFor and *ngSwitch structural directives.
-*/
+If we try to use two structural directives applied to the same template elements
+(the div in our case) we will fail.
+So as a workaround we add extra div element.
+ */
 @Component({
 	selector: 'app-root',
 	template: `
-		<ng-template [ngIf]="lessons" [ngIfElse]="loading">
-			<div class="lessons-list">
-				<button class="tab-button" (click)="login()">{{loginText}}</button>
-				<button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+		<div *ngIf="lessons">
+			<div class="lesson" *ngFor="let lesson of lessons">
+				<div class="lesson-detail">
+					{{lesson | json}}
+				</div>
 			</div>
-		</ng-template>
-		<ng-template #loading>
-			<div>Loading...</div>
-		</ng-template>
+		</div>
 	`})
 export class AppComponent {
 
