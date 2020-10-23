6f5fa43038aabc02ea2cd7a2939a0c9fc8cc172f
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 16:46:11 +0300

    Added ng-template as element and description of breaking down of *ngIf *ngFor and *ngSwitch structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-//
-// Since ng-template doesn't render itself we will get the empty screen.
-//
+
 @Component({
 	selector: 'app-root',
 	template: `
-		<ng-template>
-			<button class="tab-button" (click)="login()">{{loginText}}</button>
-			<button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+		<ng-template [ngIf]="lessons" [ngIfElse]="loading">
+			<div class="lessons-list">
+				<button class="tab-button" (click)="login()">{{loginText}}</button>
+				<button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+			</div>
 		</ng-template>
-	`
-})
+		<ng-template #loading>
+			<div>Loading...</div>
+		</ng-template>
+	`})
 export class AppComponent {
 
 	loginText = 'Login';
