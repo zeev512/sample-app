6b484909ff8b90e718aa883ca921f3f48a962e84
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 16:46:32 +0300

    Added ng-template as element and description of breaking down of *ngIf *ngFor and *ngSwitch structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 
+/*
+When we use *ngIf structural directive the Angular internally desugars the more concise
+*ngIf structural directive syntax. Let's break down what happened during the desugaring:
+- The element onto which the structural directive ngIf was applied has been moved into an ng-template
+- The expression of *ngIf has been split up and applied to two separate directives, using the [ngIf] and [ngIfElse]
+  template input variable syntax
+Exactly the same process occurs with with *ngFor and *ngSwitch structural directives.
+*/
 @Component({
 	selector: 'app-root',
 	template: `
